318b4560de19fe122ca102d8d780ff093e31c966
Ken Loh <Kenneth_LJS@Live.com>
2016-09-06 17:36:11 +0800

    Revert "Encapsulate feedbackToUser in CommandResult class."
    This reverts commit 746d870502b3562bf3ea053e34cee3bfd4d779a1.

--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -119,7 +119,7 @@ public class TextUi {
         if(resultPersons.isPresent()) {
             showPersonListView(resultPersons.get());
         }
-        showToUser(result.getFeedbackToUser(), DIVIDER);
+        showToUser(result.feedbackToUser, DIVIDER);
     }
 
     /**
