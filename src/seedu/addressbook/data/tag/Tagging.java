c7dd966b35168bc63acbc4e940ea1906a9279156
Ken Loh <Kenneth_LJS@Live.com>
2016-09-06 16:48:30 +0800

    Implement Tagging functions.

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -3,8 +3,33 @@ package seedu.addressbook.data.tag;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 public class Tagging {
+	
+	private ReadOnlyPerson person;
+	private Tag tag;
+	private TaggingAction action;
+	
+	public Tagging(ReadOnlyPerson person, Tag tag, TaggingAction action) {
+		this.person = person;
+		this.tag = tag;
+		this.action = action;
+	}
 
-	ReadOnlyPerson person;
-	Tag tag;
+	public ReadOnlyPerson getPerson() {
+		return person;
+	}
+	
+	public Tag getTag() {
+		return tag;
+	}
+	
+	public TaggingAction getAction() {
+		return action;
+	}
+	
+	public String toString() {
+		return action.toString() + " " +
+				person.getName().fullName + " " +
+				"[" + tag.tagName + "]";
+	}
 	
 }
